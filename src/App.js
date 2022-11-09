b356dab849602835c71a6ec6e41a649b2aba466d
kirang321 <kirangangudre@gmail.com>
2022-11-09 23:31:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
-import MensSaloon from './Kiran_Fw19_0321/MensPage';
-import SaloonPrime from './Kiran_Fw19_0321/MensPage';
 
 function App() {
   return (
     <div className="App">
-      <MensSaloon/>
-      {/* <SaloonPrime/> */}
+
     </div>
   );
 }
