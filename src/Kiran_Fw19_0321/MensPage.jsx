f066277bf20b43635266d0389c9013f6fae441b5
kirang321 <kirangangudre@gmail.com>
2022-11-10 10:50:14 +0530

    update

--- src/Kiran_Fw19_0321/MensPage.jsx
+++ src/Kiran_Fw19_0321/MensPage.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import { VStack, Box, HStack, Button } from "@chakra-ui/react"
+import { VStack, Box, HStack, Button, Stack, Text } from "@chakra-ui/react"
 import { AspectRatio } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
+import { IoIosPricetag } from "react-icons/io"
 
 
 const MensSaloon = () => {
     return <div>
-        <VStack spacing="25px">
-            <Box w="75%" h="400px" border="1px solid">
+        <VStack spacing="25px" >
+            <Box w="75%" h="500px" border="1px solid"  >
                 <HStack>
-                    <Box w="50%" h="400px" border="1px solid red">
-                        <Button bg="black" color="white">UC safe</Button>
-                        <h1 h="30px">Saloon for Men</h1>
+                    <Box
+                        w="50%"
+                        h="500px"
+                        border="1px solid red"
+                        pt="200px"
+                       >
+                        
+                        <Button
+                            bg="white"
+                            color="black"
+                            height="auto"
+                            w="150px"
+                            fontSize="25px"
+                            margin="auto"
+                            padding="8px"
+                            font-weight="500"
+                            text-align="start"
+                            border="1px solid gray"
+                        ># UC Safe
+                        </Button>
+                        <h1 w="300px">Saloon for Men</h1>
+                        <p >4.75(974k)</p>
                     </Box>
-                    <Box w="50%" h="400px" border="1px solid blue">
-                        <AspectRatio maxW='400px' ratio={1}>
+                    <Box w="50%" h="500px" border="1px solid blue">
+                        <AspectRatio maxW='full' ratio={1.5} alignContent="center" margin="auto" pt="130px" >
                             <iframe
                                 title='naruto'
                                 src="./video/men.mp4"
@@ -23,57 +43,77 @@ const MensSaloon = () => {
                         </AspectRatio>
                     </Box>
                 </HStack>
-                <HStack spacing='24px'>
-                    <Box w='100px' h='100px' bg='yellow.200'>
+                <HStack spacing='10px' marginTop="30px" marginBottom="30px">
+                    <Box w='100px' h='100px' >
                         <Box boxSize='100%'>
-                            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                            <Image borderRadius="10%" textAlign="center" margin="auto" src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/luminosity/1646814042922-25f8f1.jpeg' alt='Dan Abramov' />
+                            Packges
                         </Box>
-                        Packges
+
                     </Box>
-                    <Box w='100px' h='100px' bg='tomato'>
-                    <Box boxSize='100%'>
-                            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                    <Box w='100px' h='100px' >
+                        <Box boxSize='100%'>
+                            <Image borderRadius="10%" textAlign="center" margin="auto" src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1631190374864-78f3e8.jpeg' alt='Dan Abramov' />
+                            Haircut for Men & Kid's
                         </Box>
-                        Haircut for Men & Kid's
+
                     </Box>
-                    <Box w='100px' h='100px' bg='pink.100'>
-                    <Box boxSize='100%'>
-                            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                    <Box w='100px' h='100px' >
+                        <Box boxSize='100%'>
+                            <Image borderRadius="10%" textAlign="center" margin="auto" src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1631189933595-01692d.png' alt='Dan Abramov' />
+                            Hair Color
                         </Box>
-                        Hair Color
+
                     </Box>
-                    <Box w='100px' h='100px' bg='yellow.200'>
-                    <Box boxSize='100%'>
-                            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                    <Box w='100px' h='100px' >
+                        <Box boxSize='100%'>
+                            <Image borderRadius="10%" textAlign="center" margin="auto" src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1631189957304-ea2a7b.png' alt='Dan Abramov' />
+                            Shave/Beard Grooming
                         </Box>
-                        Shave/Beard Grooming
+
                     </Box>
-                    <Box w='100px' h='100px' bg='tomato'>
-                    <Box boxSize='100%'>
-                            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                    <Box w='100px' h='100px' >
+                        <Box boxSize='100%'>
+                            <Image borderRadius="10%" textAlign="center" margin="auto" src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1631189857376-d49e47.png' alt='Dan Abramov' />
+                            Detan & Facials
                         </Box>
-                        Detan & Facials
+
                     </Box>
-                    <Box w='100px' h='100px' bg='pink.100'>
-                    <Box boxSize='100%'>
-                            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+                    <Box w='100px' h='100px'  >
+                        <Box boxSize='100%'>
+                            <Image borderRadius="10%" textAlign="center" margin="auto" src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1631189905841-237b37.png' alt='Dan Abramov' />
+                            Massage Therapy
                         </Box>
-                        Massage Therapy
+
                     </Box>
 
                 </HStack>
                 <HStack spacing="25px">
-                    <Box w="70%" h="400px" border="1px solid red">
+                    <Box w="60%" h="400px" border="1px solid red">
 
                     </Box>
-                    <Box w="30%" h="400px" border="1px solid blue">
-                        <AspectRatio maxW='400px' ratio={1}>
-                            <iframe
-                                title='naruto'
-                                src=""
-                                allowFullScreen
-                            />
-                        </AspectRatio>
+                    <Box w="40%" h="400px" border="1px solid blue" justifyContent="right">
+                        <Stack spacing={4} direction='column' align='center'>
+
+                            <HStack>
+                                <Image src="IoIosPricetag" alt="" />
+                                <Text>Save 15% on every order</Text>
+                                <br />
+                                <Text>Get Plus Now</Text>
+                            </HStack>
+                            <Button colorScheme='gray' size='lg'>
+                                Save 15% on every order
+                            </Button>
+                            <Button colorScheme='gray' size="lg">
+                                Mobikwik | ZIP (Pay Later)
+                            </Button>
+                            <Button colorScheme='gray' size='lg'>
+                                5% Simple cashback up to Rs.750
+                            </Button>
+                            <Button colorScheme='gray' size='lg'>
+                                Upto 200 cashback
+                            </Button>
+                        </Stack>
                     </Box>
                 </HStack>
             </Box>
