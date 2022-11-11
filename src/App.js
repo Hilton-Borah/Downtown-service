a375822b870238856e0ba6e239a95dbd28429bb5
Hilton Borah <hiltonborah123@gmail.com>
2022-11-12 02:46:16 +0530

    day4

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       {/* <AddProduct/> */}
       {/* <EditProduct/>   */}
-      {/* <SelectSlot/> */}
+      <SelectSlot/>
       {/* <Login/> */}
-      <BasicUsage/>
+      {/* <BasicUsage/> */}
     </div>
   );
 }
